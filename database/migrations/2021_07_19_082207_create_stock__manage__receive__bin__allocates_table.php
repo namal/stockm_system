7a61bc6cbb1c588246c5bc1c9e4789bf868ae6af
Praveena-T <IT19243672@my.sliit.lk>
2021-07-19 15:30:38 +0530

    add stock_manage_receive_bin_allocate controller model table

---
+++ database/migrations/2021_07_19_082207_create_stock__manage__receive__bin__allocates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockManageReceiveBinAllocatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock__manage__receive__bin__allocates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->double('rereceivedTotalQuantity');
+            $table->string('reallocatedRack');
+            $table->string('reallocatedBin');
+            $table->string('rereceivedBatchNo');
+            $table->double('rereceivedQuantity');
+            $table->integer('rereceivedRollsCount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock__manage__receive__bin__allocates');
+    }
+}
